3ee88e9b8ccfc67fd35c2f55de0ba7218884a488
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-17 00:39:23 +0200

    Added function add_edge to build graph and its reverse graph

--- strongly_connected.py
+++ strongly_connected.py
@@ -5,18 +5,40 @@ import sys
 sys.setrecursionlimit(200000)
 
 
+
+def add_edge(graph, reverse_graph, vertex1, vertex2):
+    """Build a directed graph as well as the reverse of the directed graph.
+
+    The edges are reversed, so from input vertices, the neighbour vertex
+    becomes the key vertex and key becomes neighbour.
+
+    Parameters
+    ----------
+    graph, reverse_graph: dictionary
+        Empty dictionaries to build directed graph and it's reverse.
+    vertex1, vertex: int
+        integer values representing key vey vertex and it's too neighbour.
+    """
+    
+    rev_vert1, rev_vert2 = (vertex2, vertex1)
+    if vertex1 in graph:
+        if vertex2 not in graph[vertex1]:
+            graph[vertex1].append(vertex2)
+            reverse_graph[rev_vert1].append(rev_vert2)
+
+
 def number_of_strongly_connected_components(adj):
     result = 0
     #write your code here
     return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    print(number_of_strongly_connected_components(adj))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+    rev_graph = {vertex: [] for vertex in range(1, n+1)}
+        
+    for _ in range(m):
+        vert1, vert2 = map(int, sys.stdin.readline().split())
+        add_edge(graph, rev_graph, vert1, vert2)
+        add_edge(graph, rev_graph, vert1, vert2)
+    # print(number_of_strongly_connected_components(adj))
